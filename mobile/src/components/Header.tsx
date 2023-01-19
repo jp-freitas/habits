af021af598ee35e6ddf006bef888dedc4f9ebb8f
TecJP <tec.joao.freitas@gmail.com>
2023-01-19 09:48:41 -0300

    Creating the Habit, NewHabit screens and it's components

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,14 +1,17 @@
 import { View, TouchableOpacity, Text } from "react-native";
+import { useNavigation } from '@react-navigation/native'
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
 
 import Logo from '../assets/logo.svg'
 
 export function Header() {
+  const { navigate } = useNavigation()
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
-      <TouchableOpacity activeOpacity={0.7} className="h-11 border border-violet-500 rounded-lg px-4 flex-row items-center">
+      <TouchableOpacity onPress={() => navigate('new-habit')} activeOpacity={0.7} className="h-11 border border-violet-500 rounded-lg px-4 flex-row items-center">
         <Feather
           name="plus"
           color={colors.violet[500]}
---
+++ mobile/src/screens/Habit.tsx
@@ -0,0 +1,46 @@
+import { useRoute } from "@react-navigation/native";
+import { ScrollView, Text, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+import dayjs from 'dayjs'
+import { ProgressBar } from "../components/ProgressBar";
+import { Checkbox } from "../components/Checkbox";
+
+interface HabitParams {
+  date: string;
+}
+
+export function Habit() {
+  const route = useRoute()
+  const { date } = route.params as HabitParams
+  const parsedDate = dayjs(date)
+  const dayOfWeek = parsedDate.format('dddd')
+  const dayAndMonth = parsedDate.format('DD/MM')
+
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+        <ProgressBar progress={75} />
+        <View className="mt-6">
+          <Checkbox
+            title="Diárias do Genshin Impact"
+            checked={true}
+          />
+          <Checkbox
+            title="Gastar 160 resisnas no Genshin Impact"
+            checked={false}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
